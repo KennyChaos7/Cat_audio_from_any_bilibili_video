938f97c943e2ce858ec9098bacd713f5ebad8ced
o1 <empty@empty.com>
2024-09-05 17:46:49 +0800

    1.修改逻辑 2.增加tqdm库做下载进度条

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+from tqdm import tqdm
 import wbi as API
 import requests
 from PIL import Image
@@ -11,29 +12,37 @@ if os.path.exists(path + "/output") is False:
 path += "/output/"
 
 
-def download(url: str, file_name: str):
-    # if os.path.exists(path + file_name):
-    #     file_name = "复制 " + file_name
+def download(url: str, file_name: str, chunk_size: int):
     print(file_name + " downloading .... ")
-    file_cache = requests.get(
-                url=url,
-                headers=API.req_headers
-            )
-    tmp_file_name = str(round(time.time()))
-    if ".m4a" in file_name:
-        tmp_file_name += ".m4a"
-    elif ".png" in file_name:
-        tmp_file_name += ".png"
-    with open(path + tmp_file_name, 'wb') as file:
-        file.write(file_cache.content)
-    if ".m4a" in file_name:
-        print(file_name + " transcoding .... ")
-        os.system("ffmpeg -i " + path + tmp_file_name + " -acodec libmp3lame -ac 2 -ab 128k -id3v2_version 3 " + path + tmp_file_name[0:-4] + ".mp3")
-        os.rename(path + tmp_file_name[0:-4] + ".mp3", path + file_name[0:-4] + ".mp3")
-        # os.remove(path + tmp_file_name[0:-4] + ".m4a")
-        os.rename(path + tmp_file_name[0:-4] + ".m4a", path + file_name[0:-4] + ".m4a")
-    else:
-        os.rename(path + tmp_file_name[0:-4] + ".png", path + file_name[0:-4] + ".png")
+    resp = requests.get(
+        url=url,
+        headers=API.req_headers,
+        stream=True
+    )
+    with open(path + file_name, 'wb') as file, tqdm(
+        desc=file_name,
+        total=int(resp.headers.get('content-length', 0)),
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024
+    ) as bar:
+        # start_time = time.perf_counter()
+        # max_progress = round(len(resp.content) / chunk_size)
+        # i = 0
+        for content in resp.iter_content(chunk_size=1024):
+            size = file.write(content)
+            # progress_bar(start_time, max_progress, i)
+            # i = i + 1
+            bar.update(size)
+
+
+def transcode(file_name):
+    tmp_file_name = str(round(time.time())) + ".m4a"
+    print(file_name + " transcoding .... ")
+    os.remove(path + file_name[0:-4] + ".mp3")
+    os.system("ffmpeg -i " + path + file_name + " -acodec libmp3lame -ac 2 -ab 128k -id3v2_version 3 " + path + tmp_file_name[0:-4] + ".mp3")
+    os.rename(path + tmp_file_name[0:-4] + ".mp3", path + file_name[0:-4] + ".mp3")
+    os.remove(path + file_name[0:-4] + ".m4a")
 
 
 def main(bv_id: str):
@@ -43,12 +52,19 @@ def main(bv_id: str):
         if video_url_json['code'] == 0 and str('dash') in video_url_json['data']:
             dash_data_json = json.loads(json.dumps(video_url_json['data']['dash']))
             audio_url = dash_data_json['audio'][0]['baseUrl']
-            download(url=video_json['data']['pic'], file_name=video_json['data']['title'] + ".png")
-            download(url=audio_url, file_name=video_json['data']['title'] + ".m4a")
+            download(url=video_json['data']['pic'], file_name=video_json['data']['title'] + ".png", chunk_size=1024)
+            download(url=audio_url, file_name=video_json['data']['title'] + ".m4a", chunk_size=1024 * 1024)
+            transcode(file_name=video_json['data']['title'] + ".m4a")
+
+
+def progress_bar(start_time, max_progress, progress):
+    a = "*" * progress
+    b = "." * (max_progress - progress)
+    c = (progress / max_progress) * 100
+    dur = time.perf_counter() - start_time
+    print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c, a, b, dur), end="")
+    time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    main('BV1WX4y1L7je')
-    # im = Image.open(path + 'test.png')
-    # im = im.convert('RGB')
-    # im.save(path + 'test.jpeg', "JPEG")
+    main('BV1wE4m1R7cu')
